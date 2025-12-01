2a1292779afbf41d54df2981bac275fd7a8d23d1
chaelyooo <manlangitjohnmichael36@gmail.com>
2025-12-01 08:51:38 +0800

    Add logging to serverless handler

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 const serverless = require('serverless-http');
-const app = require('../server');
 
-module.exports = serverless(app);
+console.log('Loading app module...');
+const app = require('../server');
+console.log('App module loaded successfully');
+
+const handler = serverless(app);
+
+module.exports = async (req, res) => {
+  console.log('Request received:', req.method, req.url);
+  try {
+    return await handler(req, res);
+  } catch (error) {
+    console.error('Handler error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+};
